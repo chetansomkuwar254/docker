67f722f169c820b4da1a27e6c26ec9687274834f
chetansomkuwar254 <146824400+chetansomkuwar254@users.noreply.github.com>
2024-07-26 06:39:27 +0000

    --- Auto Git Commit ---

--- docker_installation_2.sh
+++ docker_installation_2.sh
@@ -17,5 +17,4 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo systemctl
-sudo docker --version
+sudo systemctl status docker
